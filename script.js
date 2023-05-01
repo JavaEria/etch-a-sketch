b6219e6cb68b3777596d230c583449c13f5bf41e
Javaeria <javerianisar.1993@gmail.com>
2023-05-01 15:29:33 +1000

    Add grid effect on container div

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 60; i++) {
-  let grid = document.createElement("div");
-  grid.classList.add("box");
-  container.appendChild(grid);
+  let row = document.createElement("div");
+  row.classList.add("row");
+  for (let j = 0; j < 60; j++) {
+    let grid = document.createElement("div");
+    grid.classList.add("box");
+    row.appendChild(grid);
+  }
+  container.appendChild(row);
 }
