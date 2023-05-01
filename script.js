29276b50b65b5546483dbb735a7984017aa4ff29
Javaeria <javerianisar.1993@gmail.com>
2023-05-01 14:35:57 +1000

    Add style class and and 60 box

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 60; i++) {
-  for (let j = 0; j < 60; j++) {
-    let grid = document.createElement("div");
-    container.appendChild(grid);
-  }
+  let grid = document.createElement("div");
+  grid.classList.add("box");
+  container.appendChild(grid);
 }
