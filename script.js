2d13672b442f0870223d75b6a6c0ff44047ecb18
Javaeria <javerianisar.1993@gmail.com>
2023-05-02 14:54:04 +1000

    add hover effect on boxes

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const container = document.querySelector(".container");
+let numberSquares = 60;
 
-for (let i = 0; i < 60; i++) {
+for (let i = 0; i < numberSquares; i++) {
   let row = document.createElement("div");
   row.classList.add("row");
-  for (let j = 0; j < 60; j++) {
+  for (let j = 0; j < numberSquares; j++) {
     let grid = document.createElement("div");
     grid.classList.add("box");
     row.appendChild(grid);
   }
   container.appendChild(row);
 }
+
+function random() {
+  return Math.floor(Math.random() * 255);
+}
+
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((box) => {
+  box.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+  });
+});
