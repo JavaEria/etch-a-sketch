a948e7b646fba477c4d7d6901225fdcbc4d5b915
Javaeria <javerianisar.1993@gmail.com>
2023-05-04 11:26:17 +1000

    add prompt and refresh buttons

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 const container = document.querySelector(".container");
-let numberSquares = 60;
+let numberSquares = 16;
 
-for (let i = 0; i < numberSquares; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let j = 0; j < numberSquares; j++) {
-    let grid = document.createElement("div");
-    grid.classList.add("box");
-    row.appendChild(grid);
+function drawGrids() {
+  for (let i = 0; i < numberSquares; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    for (let j = 0; j < numberSquares; j++) {
+      let grid = document.createElement("div");
+      grid.classList.add("box");
+      row.appendChild(grid);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
+}
+
+function redrawGrids() {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  drawGrids();
+  addHoverListeners();
 }
 
 function random() {
   return Math.floor(Math.random() * 255);
 }
 
-const boxes = document.querySelectorAll(".box");
-boxes.forEach((box) => {
-  box.addEventListener("mouseover", (event) => {
-    event.target.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+function addHoverListeners() {
+  const boxes = document.querySelectorAll(".box");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = `rgb(${random()}, ${random()}, ${random()})`;
+    });
   });
+}
+
+drawGrids();
+addHoverListeners();
+
+const gridButton = document.querySelector(".option");
+gridButton.addEventListener("click", (event) => {
+  gridNumber = prompt("Enter Grid Squares per Side value (10-100):", 60);
+  if (gridNumber >= 10 && gridNumber <= 100) {
+    numberSquares = gridNumber;
+    redrawGrids();
+  }
+});
+
+const refreshButton = document.querySelector(".refresh");
+refreshButton.addEventListener("click", () => {
+  window.location.reload();
 });
